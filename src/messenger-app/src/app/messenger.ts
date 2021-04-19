397ad2f8ea1ac6a72ee7ba2b9c83a563417a489e
( <tgilbert@fastmail.com.au>
2021-04-19 16:45:03 +1000

    added loadFromJSON method + various fixes

--- src/messenger-app/src/app/messenger.ts
+++ src/messenger-app/src/app/messenger.ts
@@ -2,19 +2,26 @@ export class User {
     username: string;
     profilePicture: ArrayBuffer;
 
-    constructor(username: string, profilePicture: ArrayBuffer) {
+    public constructor(username: string, profilePicture: ArrayBuffer) {
         this.username = username;
         this.profilePicture = profilePicture;
     }
 
-    async getPfpDataURL(): Promise<string> {
-        let blob = new Blob([this.profilePicture]);
+    public async getPfpDataURL(): Promise<string> {
+        const blob = new Blob([this.profilePicture]);
 
         return new Promise(resolve => {
-            let reader = new FileReader();
+            const reader = new FileReader();
             reader.onloadend = () => resolve(<string>reader.result); // cast result to string (because it is a data url) and resolve promise
             reader.readAsDataURL(blob); // read the key as a data url
         });
+    }
+
+    public loadFromJSON(json: string) {
+        const jsonUser = JSON.parse(json);
+        return new User(
+            jsonUser.username, jsonUser.profilePicture
+        );
     }
 }
 
