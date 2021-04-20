e6346861ad2a6bd63f744bd7a219c5b4e5deddbe
( <tgilbert@fastmail.com.au>
2021-04-20 20:08:02 +1000

    added working form

--- src/messenger-app/src/app/register/register.component.ts
+++ src/messenger-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MessengerService } from '../messenger.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private messenger: MessengerService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log("submitted");
+    this.messenger.register(this.registerForm.value["username"]);
+  }
 }
