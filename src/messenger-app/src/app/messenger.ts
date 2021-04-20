d6da2291efba9fd3322ad8f6923d520b142a053b
( <tgilbert@fastmail.com.au>
2021-04-20 22:11:03 +1000

    added messages

--- src/messenger-app/src/app/messenger.ts
+++ src/messenger-app/src/app/messenger.ts
@@ -1,16 +1,20 @@
 export class User {
     public username: string;
     public pfpDataURL: string;
+    public messages: Message[];
 
-    public constructor(username: string, pfpDataURL?: string) {
+    public constructor(username: string, pfpDataURL?: string, messages?: Message[]) {
         this.username = username;
         this.pfpDataURL = pfpDataURL ?? "";
+        this.messages = messages ?? [];
     }
 
     public static loadFromJSON(json: string): User {
         const jsonUser = JSON.parse(json);
         return new User(
-            jsonUser.username, jsonUser.pfpDataURL
+            jsonUser.username,
+            jsonUser.pfpDataURL,
+            jsonUser.messages
         );
     }
 }
