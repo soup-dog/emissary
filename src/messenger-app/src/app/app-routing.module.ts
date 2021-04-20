f0a13b391a6b1b9dcfe68eeb5894284c546396b9
( <tgilbert@fastmail.com.au>
2021-04-20 21:46:04 +1000

    added routes

--- src/messenger-app/src/app/app-routing.module.ts
+++ src/messenger-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { AppComponent } from './app.component';
+import { MessengerComponent } from './messenger/messenger.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', component: MessengerComponent },
+  { path: 'app.html', component: MessengerComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'register.html', component: RegisterComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'account.html', component: AccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
