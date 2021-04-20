2f4690b30c5311398462c7af97b861ced585520d
( <tgilbert@fastmail.com.au>
2021-04-20 17:48:59 +1000

    various changes

--- src/messenger-app/src/app/messenger.service.ts
+++ src/messenger-app/src/app/messenger.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { User } from './messenger';
 import { NormalEvent } from './normal-event';
 
@@ -8,21 +8,36 @@ import { NormalEvent } from './normal-event';
 })
 export class MessengerService {
   public static readonly USER_STORAGE_KEY = "user";
-  private _user: User | null;
+  private _user: User | null = null;
   public userSet: NormalEvent<User> = new NormalEvent<User>();
-
-  constructor() {
-    this._user = this.getUser();
+  public get storedUserAvailable(): boolean {
+    return sessionStorage.getItem(MessengerService.USER_STORAGE_KEY) != null;
   }
 
-  public setUserPfp(profilePicture: ArrayBuffer): void {
-    if (this._user == null) { return; }
-    this._user.profilePicture = profilePicture;
-    this.pushUser();
+  constructor(private router: Router) {
+    if (this.storedUserAvailable) {
+      this._user = this.getUser();
+    }
+    console.log(this._user);
+  }
+
+  public setUserPfpFromFile(file: File): void {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (this._user == null) { return; }
+      this._user.pfpDataURL = <string>reader.result;
+      this.pushUser();
+    }; // cast result to string (because it is a data url) and set pfpDataURL
+    reader.readAsDataURL(file); // read the image as a data url
   }
 
   register(username: string) {
     this._user = new User(username);
+    this.pushUser();
+  }
+
+  login() {
+
   }
 
   private pullUser(): void {
@@ -34,9 +49,9 @@ export class MessengerService {
     this.setUser(this._user);
   }
 
-  getUser(): User | null {
+  getUser(): User {
     const data = sessionStorage.getItem(MessengerService.USER_STORAGE_KEY); // pull user from storage as a JSON string
-    if (data == null) { return null; } // return null if stored data is null
+    if (data == null) { throw Error("No user at " + MessengerService.USER_STORAGE_KEY + "."); } // throw error if stored data is null
     const user = User.loadFromJSON(data); // otherwise convert the JSON string to an instance of User
     return user;
   }
