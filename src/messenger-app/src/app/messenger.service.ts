ba7ebb461ae1783315ef0d66adad8ea20a7e8038
( <tgilbert@fastmail.com.au>
2021-04-21 21:18:10 +1000

    various additions and fixs
    - added Message import
    - added REGISTER_ROUTE constant
    - added loggedIn property
    - simplified constructor
    - added checkLoggedIn mehtod
    - added requiresLogin decorator
    - made null checks strict
    - made setUser private

--- src/messenger-app/src/app/messenger.service.ts
+++ src/messenger-app/src/app/messenger.service.ts
@@ -1,43 +1,67 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from './messenger';
+import { User, Message } from './messenger';
 import { NormalEvent } from './normal-event';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessengerService {
-  public static readonly USER_STORAGE_KEY = "user";
+  public static readonly USER_STORAGE_KEY: string = "user";
+  public static readonly REGISTER_ROUTE: string = "app/register";
   private _user: User | null = null;
   public userSet: NormalEvent<User> = new NormalEvent<User>();
+
   public get storedUserAvailable(): boolean {
     return sessionStorage.getItem(MessengerService.USER_STORAGE_KEY) != null;
   }
 
+  public get loggedIn() {
+    return this._user !== null;
+  }
+
   constructor(private router: Router) {
-    if (this.storedUserAvailable) {
-      this._user = this.getUser();
+    this.pullUser();
+  }
+
+  requiresLogin = (target: MessengerService, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      target.checkLoggedIn();
+      originalMethod.apply(this, args);
     }
-    console.log(this._user);
+
+    return descriptor;
+  } 
+
+  public checkLoggedIn() {
+    if (!this.loggedIn) {
+      this.router.navigate([MessengerService.REGISTER_ROUTE]);
+    };
+  }
+
+  public getMessages(): Message[] {
+    return (<User>this._user).messages;
   }
 
   public setUserPfpFromFile(file: File): void {
     const reader = new FileReader();
     reader.onloadend = () => {
-      if (this._user == null) { return; }
+      if (this._user === null) { return; }
       this._user.pfpDataURL = <string>reader.result;
       this.pushUser();
     }; // cast result to string (because it is a data url) and set pfpDataURL
     reader.readAsDataURL(file); // read the image as a data url
   }
 
-  register(username: string) {
+  public register(username: string) {
     this._user = new User(username);
     this.pushUser();
   }
-
-  login() {
-
+  
+  public login() {
+    
   }
 
   private pullUser(): void {
@@ -45,18 +69,18 @@ export class MessengerService {
   }
 
   private pushUser(): void {
-    if (this._user == null) { return; }
+    if (this._user === null) { return; }
     this.setUser(this._user);
   }
 
-  getUser(): User {
+  public getUser(): User {
     const data = sessionStorage.getItem(MessengerService.USER_STORAGE_KEY); // pull user from storage as a JSON string
     if (data == null) { throw Error("No user at " + MessengerService.USER_STORAGE_KEY + "."); } // throw error if stored data is null
     const user = User.loadFromJSON(data); // otherwise convert the JSON string to an instance of User
     return user;
   }
 
-  setUser(user: User) {
+  private setUser(user: User) {
     sessionStorage.setItem(MessengerService.USER_STORAGE_KEY, JSON.stringify(user)); // set user in session storage
     this.userSet.dispatch(user); // dispatch event
   }
