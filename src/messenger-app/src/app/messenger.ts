0e6fa92d2478a3ec88823f3339ede711498233dc
( <tgilbert@fastmail.com.au>
2021-04-22 20:00:19 +1000

    added UserInfo class + various fixes
    - added UserInfo class
    - also remembered that default parameters are a thing
    - renamed jsonUser to jsonObject

--- src/messenger-app/src/app/messenger.ts
+++ src/messenger-app/src/app/messenger.ts
@@ -3,18 +3,36 @@ export class User {
     public pfpDataURL: string;
     public messages: Message[];
 
-    public constructor(username: string, pfpDataURL?: string, messages?: Message[]) {
+    public constructor(username: string, pfpDataURL: string = "", messages: Message[] = []) {
         this.username = username;
-        this.pfpDataURL = pfpDataURL ?? "";
-        this.messages = messages ?? [];
+        this.pfpDataURL = pfpDataURL;
+        this.messages = messages;
     }
 
     public static loadFromJSON(json: string): User {
-        const jsonUser = JSON.parse(json);
+        const jsonObject = JSON.parse(json);
         return new User(
-            jsonUser.username,
-            jsonUser.pfpDataURL,
-            jsonUser.messages
+            jsonObject.username,
+            jsonObject.pfpDataURL,
+            jsonObject.messages
+        );
+    }
+}
+
+export class UserInfo {
+    public username: string;
+    public pfpDataURL: string;
+
+    public constructor(username: string = "", pfpDataURL: string = "") {
+        this.username = username;
+        this.pfpDataURL = pfpDataURL;
+    }
+
+    public static fromJSON(json: string): User {
+        const jsonObject = JSON.parse(json);
+        return new User(
+            jsonObject.username,
+            jsonObject.pfpDataURL
         );
     }
 }
