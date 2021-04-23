02f95127e735ff8387ddce2a23d6650b983fc58f
( <tgilbert@fastmail.com.au>
2021-04-23 21:41:58 +1000

    added register popup with key download

--- src/messenger-app/src/app/register/register.component.ts
+++ src/messenger-app/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { MessengerService } from '../messenger.service';
 
 @Component({
@@ -11,13 +13,23 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup = new FormGroup({
     username: new FormControl('')
   });
+  username: string = "";
+  shown: boolean = false;
+  keyDataURL: SafeUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // rickroll anyone who tries to download the key file before registering
 
-  constructor(private messenger: MessengerService) { }
+  constructor(private messenger: MessengerService, private sanitiser: DomSanitizer, private router: Router) { }
 
   ngOnInit(): void {
   }
+  
+  public requireUser = this.messenger.requireSession;
 
   onSubmit(): void {
-    this.messenger.register(this.registerForm.value["username"]);
+    this.messenger.register(this.registerForm.value["username"])
+      .then((keyDataURL) => {
+        this.keyDataURL = this.sanitiser.bypassSecurityTrustUrl(keyDataURL); // set key data url
+        this.username = this.messenger.requireSession().username; // set username
+        this.shown = true; // show popup
+      });
   }
 }
