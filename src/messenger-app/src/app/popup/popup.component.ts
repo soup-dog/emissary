0afe2dea433e79a3b98ee63559ee44c3ad41ff2e
( <tgilbert@fastmail.com.au>
2021-04-23 21:42:21 +1000

    made popup work

--- src/messenger-app/src/app/popup/popup.component.ts
+++ src/messenger-app/src/app/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-popup',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent implements OnInit {
+  @Input() shown: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
