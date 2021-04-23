0c2a35a67138910a686e3abf4e8c90f9ef1e700b
( <tgilbert@fastmail.com.au>
2021-04-23 16:28:47 +1000

    added login route

--- src/messenger-app/src/app/app-routing.module.ts
+++ src/messenger-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { MessengerComponent } from './messenger/messenger.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -12,6 +12,8 @@ const routes: Routes = [
   { path: 'register.html', component: RegisterComponent },
   { path: 'account', component: AccountComponent },
   { path: 'account.html', component: AccountComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'login.html', component: LoginComponent }
 ];
 
 @NgModule({
