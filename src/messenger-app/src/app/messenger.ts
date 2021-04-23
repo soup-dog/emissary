a728baae492993f4fb2b91cc4d9a3e4216db1fc9
( <tgilbert@fastmail.com.au>
2021-04-23 12:12:39 +1000

    added UserKey class and routes to User

--- src/messenger-app/src/app/messenger.ts
+++ src/messenger-app/src/app/messenger.ts
@@ -2,11 +2,13 @@ export class User {
     public username: string;
     public pfpDataURL: string;
     public messages: Message[];
+    public routes: Route[];
 
-    public constructor(username: string, pfpDataURL: string = "", messages: Message[] = []) {
+    public constructor(username: string, pfpDataURL: string = "", messages: Message[] = [], routes: Route[] = []) {
         this.username = username;
         this.pfpDataURL = pfpDataURL;
         this.messages = messages;
+        this.routes = routes;
     }
 
     public static fromJSON(json: string): User {
@@ -14,8 +16,38 @@ export class User {
         return new User(
             jsonObject.username,
             jsonObject.pfpDataURL,
-            jsonObject.messages
+            jsonObject.messages,
+            jsonObject.routes
         );
+    }
+}
+
+export class UserKey {
+    public static readonly keyGenAlgorithm = {
+        name: 'AES-CBC', // aes in cipher block chaining mode
+        length: 128 // 128 bit key
+    };
+    encryptionAlgorithm: any;
+    cryptoKey: CryptoKey;
+
+    public constructor(cryptoKey: CryptoKey, iv: BufferSource) {
+        this.cryptoKey = cryptoKey;
+        this.encryptionAlgorithm = {
+            name: 'AES-CBC', // aes in cipher block chaining mode
+            iv: iv // initialisation vector
+        };
+    }
+
+    public async encrypt(user: User): Promise<ArrayBuffer> {
+        const encoder = new TextEncoder();
+
+        return await window.crypto.subtle.encrypt(this.encryptionAlgorithm, this.cryptoKey, encoder.encode(JSON.stringify(user)));
+    }
+    
+    public async decrypt(buffer: ArrayBuffer): Promise<User> {
+        const decoder = new TextDecoder();
+
+        return User.fromJSON(decoder.decode(await window.crypto.subtle.decrypt(this.encryptionAlgorithm, this.cryptoKey, buffer)))
     }
 }
 
@@ -47,7 +79,7 @@ export class Route {
     public static fromJSON(json: string): Route {
         const jsonObject = JSON.parse(json);
         return new Route(
-            jsonObject.userInfo
+            UserInfo.fromJSON(jsonObject.userInfo)
         );
     }
 }
