65e19c7674e7882a7300e00c405be85587b6a8e2
( <tgilbert@fastmail.com.au>
2021-04-25 09:02:11 +1000

    removed debug code + added comments + added more info to the docstring

--- src/messenger-app/src/app/base-converter.ts
+++ src/messenger-app/src/app/base-converter.ts
@@ -3,35 +3,29 @@ function baseLog(x: number, base: number) {
 }
 
 /**
- * 
- * @param n number to convert as an array of the value in each place. e.g. [1, 0, 1] for 5 in binary
+ * Converts a number between bases.
+ * @param n number to convert as an Uint8Array of the value in each place. e.g. [1, 0, 1] for 5 in binary
  * @param from_base the base to convert from. e.g. 16 for hexadecimal
  * @param to_base the base to convert to. e.g. 2 for binary
- * @returns 
+ * @returns n in base to_base as an Uint8Array.
  */
 export function convertBase(n: Uint8Array, from_base: number, to_base: number) {
     let total: number = 0;
 
     for (let i = 0; i < n.length; i++) {
-        console.log(Math.pow(from_base, i));
-        total += n[i] * Math.pow(from_base, (n.length - 1) - i);
+        total += n[i] * Math.pow(from_base, (n.length - 1) - i); // convert each place to its total value and and it to the totoal
     }
 
     console.log(total);
 
     if (total === 0) { return new Uint8Array(0); }
 
-    const digits = Math.ceil(baseLog(total, to_base));
+    const digits = Math.ceil(baseLog(total, to_base)); // find how long result is going to be
     const result = new Uint8Array(digits);
     let remaining = total;
-    console.log(digits);
-    console.log(baseLog(total, to_base))
-    console.log(result);
-    console.log(remaining);
-    console.log('start');
     for (let i = 0; i < result.length; i++) {
-        result[i] = remaining % to_base;
-        remaining = Math.floor(remaining / to_base);
+        result[i] = remaining % to_base; // store the remainder in result
+        remaining = Math.floor(remaining / to_base); // explode as many dots as possible
     }
 
     return result;
