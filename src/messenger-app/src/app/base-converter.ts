e360202962041fcd4d09152c2767fe3aa0aa08ff
( <tgilbert@fastmail.com.au>
2021-04-25 11:30:34 +1000

    fixed several bugs
    - reversed result
    - uint8 -> uint16
    - Math.ceil -> Math.floor(n + 1) very similar but fixes bug when total == to_base raised to an integer

--- src/messenger-app/src/app/base-converter.ts
+++ src/messenger-app/src/app/base-converter.ts
@@ -9,24 +9,30 @@ function baseLog(x: number, base: number) {
  * @param to_base the base to convert to. e.g. 2 for binary
  * @returns n in base to_base as an Uint8Array.
  */
-export function convertBase(n: Uint8Array, from_base: number, to_base: number) {
+export function convertBase(n: Uint16Array, from_base: number, to_base: number): Uint16Array {
     let total: number = 0;
 
     for (let i = 0; i < n.length; i++) {
         total += n[i] * Math.pow(from_base, (n.length - 1) - i); // convert each place to its total value and and it to the totoal
     }
 
+    if (total === 0) { return new Uint16Array(0); }
     console.log(total);
+    console.log(from_base);
+    console.log(to_base);
 
-    if (total === 0) { return new Uint8Array(0); }
-
-    const digits = Math.ceil(baseLog(total, to_base)); // find how long result is going to be
-    const result = new Uint8Array(digits);
+    const digits = Math.floor(baseLog(total, to_base) + 1); // find how long result is going to be
+    console.log(digits);
+    const result = new Uint16Array(digits);
     let remaining = total;
     for (let i = 0; i < result.length; i++) {
+        console.log("new round");
+        console.log(remaining);
         result[i] = remaining % to_base; // store the remainder in result
         remaining = Math.floor(remaining / to_base); // explode as many dots as possible
+        console.log(result[i]);
+        console.log(remaining);
     }
 
-    return result;
+    return result.reverse();
 }
