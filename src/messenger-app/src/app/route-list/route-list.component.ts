44993fbe0a4608195ee6eb8a26fc80c1af03475f
( <tgilbert@fastmail.com.au>
2021-04-26 08:18:13 +1000

    added content to route-list

--- src/messenger-app/src/app/route-list/route-list.component.ts
+++ src/messenger-app/src/app/route-list/route-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessengerService } from '../messenger.service';
 
 @Component({
   selector: 'app-route-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./route-list.component.scss']
 })
 export class RouteListComponent implements OnInit {
+  showNewRoutePopup: boolean = false;
+  newRouteWordKey: string = "";
 
-  constructor() { }
+  constructor(private messenger: MessengerService) { }
 
   ngOnInit(): void {
   }
 
+  get routes() {
+    return this.messenger.getRoutes();
+  }
+
+  onNewRouteClick(): void {
+    this.showNewRoutePopup = true;
+  }
 }
