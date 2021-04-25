29e7ab03717f2648f5c09786f7d552c5f37ff5c8
( <tgilbert@fastmail.com.au>
2021-04-25 14:15:08 +1000

    removed debug code

--- src/messenger-app/src/app/base-converter.ts
+++ src/messenger-app/src/app/base-converter.ts
@@ -1,9 +1,5 @@
 import Big, { RoundingMode } from 'big.js';
 
-function baseLog(x: number, base: number) {
-    return Math.log(x) / Math.log(base) // https://en.wikipedia.org/wiki/Logarithm#Change_of_base
-}
-
 /**
  * Converts a number between bases.
  * @param n number to convert as an Uint8Array of the value in each place. e.g. [1, 0, 1] for 5 in binary
@@ -21,25 +17,16 @@ export function convertBase(n: Uint32Array, fromBase: number, toBase: number): U
     }
 
     if (total.eq(0)) { return new Uint32Array(0); }
-    console.log(total.toString());
-    console.log(fromBase);
-    console.log(toBase);
 
     const result = [];
     while (total.gte(toBase)) {
-        console.log("new round");
-        console.log(total.toString());
         result.push(total.mod(toBase)); // store the remainder in result
         total = total.div(toBase).round(0, RoundingMode.RoundDown); // explode as many dots as possible
-        console.log(result[result.length - 1].toString());
-        console.log(total.toString());
-        console.log(total.toString(), new Big(toBase).toString(), total.gt(toBase));
     }
 
     result.push(total);
 
     const numResult = result.reverse().map(big => big.toNumber());
-    console.log(numResult);
 
     return new Uint32Array(numResult);
 }
