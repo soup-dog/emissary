87e7de4979e771470ceb4d9d4907db1994728b2a
( <tgilbert@fastmail.com.au>
2021-04-26 13:28:59 +1000

    removed redundant import

--- src/messenger-app/src/app/message-list/message-list.component.ts
+++ src/messenger-app/src/app/message-list/message-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Message, User } from '../messenger';
+import { Message } from '../messenger';
 
 @Component({
   selector: 'app-message-list',
