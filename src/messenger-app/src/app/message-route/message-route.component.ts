e8e8a5f0ab8dbd768e03c4d36f9b05beded5a4ac
( <tgilbert@fastmail.com.au>
2021-04-26 13:29:10 +1000

    added send message form

--- src/messenger-app/src/app/message-route/message-route.component.ts
+++ src/messenger-app/src/app/message-route/message-route.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Route } from '../messenger';
 import { MessengerService } from '../messenger.service';
@@ -10,13 +11,23 @@ import { MessengerService } from '../messenger.service';
   styleUrls: ['./message-route.component.scss']
 })
 export class MessageRouteComponent implements OnInit {
-  messageRoute: Route | null = null;
+  public messageRoute: Route | null = null;
+  public routeIndex: number = -1;
+  public messageForm: FormGroup = new FormGroup({
+    message: new FormControl('')
+  });
 
   constructor(private route: ActivatedRoute, private messenger: MessengerService) { }
 
   ngOnInit(): void {
-    const routeIndex = Number(this.route.snapshot.paramMap.get('routeIndex'));
-    this.messageRoute = this.messenger.requireSession().user.routes[routeIndex];
+    this.routeIndex = Number(this.route.snapshot.paramMap.get('routeIndex'));
+    this.messageRoute = this.messenger.requireSession().user.routes[this.routeIndex];
+  }
+
+  onMessageFormSubmit(): void {
+    console.log("in onsubmit");
+    this.messenger.sendMessage(this.messageForm.value["message"], this.routeIndex);
+    this.messageForm.value["message"] = "";
   }
 
 }
